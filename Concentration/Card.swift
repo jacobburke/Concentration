6c5a0099407e07f75fad7d92d3627c0bcb7ed7b0
Jacob Burke <jacobxburke@icloud.com>
2017-11-15 22:55:16 -0600

    Add two Models to project: Concentration and Card.

---
+++ Concentration/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Jacob Burke on 11/15/17.
+//  Copyright © 2017 Jacob Burke. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    
+    var isFaceUp = false
+    var isFaceDown = false
+    var identifier: Int
+    
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,24 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Jacob Burke on 11/15/17.
+//  Copyright © 2017 Jacob Burke. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    
+  
+    
+    var cards = [Card]()
+    
+    func chooseCard(at index: Int) {
+        
+        
+    }
+    
+    
+}
