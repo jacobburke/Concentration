fbafce33a21ff1097f219118bcca87a523db2878
Jacob Burke <jacobxburke@icloud.com>
2017-11-15 19:16:42 -0600

    Refactoring code

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,38 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var flipCount = 0
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
     
     @IBOutlet weak var flipCountLabel: UILabel!
     
-    
-    
-    
+    @IBOutlet var cardButtons: [UIButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        flipCountLabel.text = "Flips: \(flipCount)"
-        flipCard(withEmoji: "👻", on: sender)
+        let cardNumber = cardButtons.index(of: sender)!
+        print("cardNumber = \(cardNumber)")
         
-    
     }
     
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCount += 1
-        flipCountLabel.text = "Flips: \(flipCount)"
-        flipCard(withEmoji: "🎃", on: sender)
-    
-    }
-    
-    @IBAction func touchThirdCard(_ sender: UIButton) {
-        flipCount += 1
-        flipCountLabel.text = "Flips: \(flipCount)"
-        flipCard(withEmoji: "⚰️", on: sender)
-    
-    }
-    
-    
-    
+
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         
         if button.currentTitle == emoji {
@@ -52,12 +38,9 @@ class ViewController: UIViewController {
             button.setTitle(emoji, for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
-        
-        
-        
-    }
-    
-    }
-    
-    
 }
+    
+    }
+        }
+    
+
